3f0f7423a41d9c71df5d0cb281685bf3c70417ea
Jong Ha Lee <jonghalee@Jongs-MacBook-Pro.local>
2016-04-04 22:26:42 -0700

    testing country occurence count, created testing folder

---
+++ js/testing/idigbioAPI_test.js
@@ -0,0 +1,63 @@
+// find mode of array
+function mode(array) {
+  if (array.length == 0) {
+    return null;
+  }
+  var modeMap = {};
+  var maxEl = array[0];
+  var maxCount = 1;
+
+  $.each(array, function(index, value) {
+    if (value != 'undefined') {
+      if (!modeMap[value]) {
+        modeMap[value] = 1;
+      } else {
+        modeMap[value]++;  
+      }
+      if(modeMap[value] > maxCount) {
+        maxEl = value;
+        maxCount = modeMap[value];
+      }
+    }
+  });
+  return maxEl;
+}
+
+function search_idigbio(query, api_dfd, results) {
+  // call idigbio service
+  $.ajax({
+    url: 'https://search.idigbio.org/v2/search/records/',
+    dataType: 'json',
+    contentType: 'application/json',
+    type: 'POST',
+    // update search limit
+    data: JSON.stringify({ limit:5, rq:{scientificname: query} })
+  }).done(function(data) {
+    // process resulting data
+    var taxon = [];
+    var count = data.itemCount;
+    var country_count = {};
+    $.each(data.items, function(index, value) {
+      var resultObject = value.indexTerms;
+
+      //Processing country count data
+      if(resultObject.country != 'undefined') {
+        country_count[resultObject.country] = 1;
+      } else {
+        country_count[resultObject.country]++;
+      }
+
+      if (resultObject.highertaxon) {
+        taxon.push(resultObject.highertaxon);
+      }
+    });
+
+    // check if there are any results
+    if (data.items.length !=  0) {
+      results['idigbio'] = {'name': query, 'taxonomy': mode(taxon), 'count': count, database: 'idigbio'};
+    }
+    
+    // notify done to controller
+    api_dfd.resolve();
+  });
+}
