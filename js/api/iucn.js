97f12de811053f37167999ec225e7fdf6b57ce8f
Calvin Dong <calvinldong@gmail.com>
2016-04-05 12:46:19 -0700

    search by location iucn

--- js/api/iucn.js
+++ js/api/iucn.js
@@ -50,12 +50,29 @@ function count_iucn(api_dfd, results) {
 
 function locationsearch_iucn(query, location, api_dfd, results) {
 	var jqXHR=$.ajax({
-	url: "http://apiv3.iucnredlist.org/api/v3/species/countries/name"+query+"?token=49a8a2b7e481a521cb4c4c5360044de9a81c5fac89bd5db1db80f733e93126db"
+	url: "http://apiv3.iucnredlist.org/api/v3/species/countries/name/"+query+"?token=49a8a2b7e481a521cb4c4c5360044de9a81c5fac89bd5db1db80f733e93126db"
 
 
 }
 ).done(function(data){
-
+		if (data.result == 0) {
+			results['iucn'] = {'count' : 0, 'location': location, 'database': 'IUCN'};
+		}
+		else {
+			var countries = data.result;
+			var has = false;
+			$.each(countries, function(i, v) {
+				if (v.country === location) {
+					results['iucn'] = {'count' : 1, 'location': location, 'database': 'IUCN'};
+					has = true;
+					break;
+				}
+			
+			});
+			if (!has) {
+				results['iucn'] = {'count' : 0, 'location': location, 'database': 'IUCN'};
+			}
+		}
 		api_dfd.resolve();
 });
 
