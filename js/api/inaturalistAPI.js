7004bfac9fa87d789d77c86fdc2d3f05c49a65c5
Natnicha Vanitchanant <Natnicha@Natnichas-MacBook-Pro.local>
2016-04-09 20:59:26 -0700

    gbif location using dict

---
+++ js/api/inaturalistAPI.js
@@ -0,0 +1,27 @@
+
+ //this function is used for keyword search.
+function search_inat(query, api_dfd, results) {
+var cleaned=[];
+
+var jqXHR=$.ajax({
+	url: "http://api.inaturalist.org/v1/observations?q="+query+"&per_page=500&order=desc&order_by=created_at"
+
+
+}
+
+	).done(function(data){
+    
+		if(data.results.length != 0) {
+			var headers = jqXHR.getResponseHeader('X-Total-Entries');
+			results['inaturalist'] = {'name': query, 'taxonomy': "not available", 'count': headers, 'database': 'iNaturalist'};
+		}
+		api_dfd.resolve();
+
+	});
+}
+
+
+
+function search_inat_location(query, location, api_dfd, results) {
+	api_dfd.resolve();
+}
