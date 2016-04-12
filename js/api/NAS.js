15b2ba3ba70813dd058a3997becd22dcb7e8529a
Calvin Dong <calvinldong@gmail.com>
2016-04-11 21:29:35 -0700

    adds nas

---
+++ js/api/NAS.js
@@ -0,0 +1,56 @@
+function search_iucn(query, api_dfd, results) {
+var cleaned=[];
+var space = query.indexOf(" ");
+var genus = "";
+var species = "";
+if (space < 0) {
+	genus = query.substring(0,space);
+	species = query.substring(space + 1);
+}
+else {
+	alert("call invalid, roy do whatever you want to handle this exception");
+}
+var jqXHR=$.ajax({
+	url: "http://nas.er.usgs.gov/api/v1/occurrence/search?genus="+genus+"&species="+species+"&api_key=7F0F12B9-BFF1-4266-A0B5-77C4115CA7B8"
+
+
+}
+
+	).done(function(data){
+    
+   
+		
+		if(data.results.length>0) {
+				var info = data.results[0];
+		 var taxon = [info.group, info.family, info.genus, info.species];
+		results['NAS'] = {'name': query, 'taxonomy': taxon.join(), 'database': 'NAS'};
+		
+		}
+	
+	api_dfd.resolve();
+	});
+
+}
+
+function count_iucn(query, api_dfd, results) {
+	var jqXHR=$.ajax({
+	url: "http://nas.er.usgs.gov/api/v1/occurrence/search?genus="+genus+"&species="+species+"&api_key=7F0F12B9-BFF1-4266-A0B5-77C4115CA7B8"
+
+
+
+}
+
+	).done(function(data){
+    
+    
+		var count = data.count;
+		
+		results['NAS'] = {'count': count, 'database': 'IUCN'};
+		
+		
+			results['NAS'] = {'count': count, 'database': 'IUCN'};
+		
+		api_dfd.resolve();
+	});
+	
+}
